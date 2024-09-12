ab99b711200e5152a4e7c8356541c95671940a02
Pintu kumar <pintu1012kumar@gmail.com>
2024-09-12 16:28:30 +0530

    update

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -22,8 +22,8 @@ function FeaturedCourses() {
         <div className="py-12 bg-gray-900">
             <div>
                 <div className="text-center">
-                    <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">FEATURED COURSES</h2>
-                    <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">Learn With the Best</p>
+                    <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase"> COURSES HERE:</h2>
+                    <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">Learn With the Best Roadmap</p>
                 </div>
             </div>
             <div className="mt-10 mx-8">
